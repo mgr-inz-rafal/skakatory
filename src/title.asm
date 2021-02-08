bfa5731399aa1131e77d84005162dee203a4b563
mgr-inz-rafal <rchabowski@gmail.com>
2021-02-08 21:40:31 +0100

    Random name for male

--- src/title.asm
+++ src/title.asm
@@ -16,9 +16,39 @@
 
 CHUJ        jmp CHUJ
 
+PICK_NUMBER_FROM_1_TO_250
+            lda RANDOM
+            sta TMP
+            #if .byte TMP < #1 .or .byte TMP > #250
+                jmp PICK_NUMBER_FROM_1_TO_250
+            #end
+            lda TMP
+            tay
+            rts
+
+; Output:
+; X = 0: Names from   0 to 250
+; X = 1: Names from 251 to 500
+; Y = Name offset
+; For example:
+; X=0, Y=15 - Pick 15th name from first part (15)
+; X=1, Y=17 - Pick 17th name from second part (268)
+PICK_RANDOM_NAME_INDEX
+            lda RANDOM
+            and #%00000001
+            tax
+            jsr PICK_NUMBER_FROM_1_TO_250
+            rts
+
 SETUP_RANDOM_NAME
-            mwa #$4000+(12*5) TMP  ; This need to point to random name, for now, it's the first one
-            mwa #$4000 TMP2
+            jsr PICK_RANDOM_NAME_INDEX
+            mwa #$4000 TMP  ; This need to point to random name, for now, it's the first one
+SRN_2       dey
+            cpy #0
+            beq SRN_1
+            adw TMP #MAX_NAME_LEN
+            jmp SRN_2
+SRN_1       mwa #$4000 TMP2
             rts
 
 SETUP_RANDOM_NAME_BABSKIE
