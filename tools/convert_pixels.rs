f957915a7ee8f835d2f03376a7d9865b2c4d38dd
mgr-inz-rafal <rchabowski@gmail.com>
2021-03-21 18:23:24 +0100

    Add frames `0..7`

--- tools/convert_pixels.rs
+++ tools/convert_pixels.rs
@@ -1,22 +1,26 @@
 use std::fs::File;
+use std::io::Write;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::io::Write;
 
 fn main() {
-    let mut file = File::create("decimal_pixel_data/frame_00_bytes.bin").unwrap();
-    if let Ok(lines) = read_lines("decimal_pixel_data/frame_00_bytes.txt") {
-        for line in lines {
-            if let Ok(ip) = line {
-                let number = ip.parse::<u8>().unwrap();
-                file.write_all(&[number]).unwrap();
+    for i in 0..=6 {
+        let mut file = File::create(format!("decimal_pixel_data/frame_{}_bytes.bin", i)).unwrap();
+        if let Ok(lines) = read_lines(format!("decimal_pixel_data/frame_{}_bytes.txt", i)) {
+            for line in lines {
+                if let Ok(ip) = line {
+                    let number = ip.parse::<u8>().unwrap();
+                    file.write_all(&[number]).unwrap();
+                }
             }
         }
-    }    
+    }
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
